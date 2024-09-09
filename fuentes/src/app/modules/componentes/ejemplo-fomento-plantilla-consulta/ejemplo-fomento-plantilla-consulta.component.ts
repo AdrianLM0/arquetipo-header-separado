37f52196a623ee7c9495f8a3e98f9d208656bade
cfernandez <cfernandez@proxya.com>
2024-09-09 09:52:18 +0200

    conectando backend con el componente plantilla consulta

--- fuentes/src/app/modules/componentes/ejemplo-fomento-plantilla-consulta/ejemplo-fomento-plantilla-consulta.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-plantilla-consulta/ejemplo-fomento-plantilla-consulta.component.ts
@@ -2,47 +2,39 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { FomentoPlantillaConsultaComponent } from '@fomento/i-rf-web-component-node-lib';
 import { Constants } from '../../../config/constants';
+import { Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
 	selector: 'app-ejemplo-fomento-plantilla-consulta',
 	templateUrl: './ejemplo-fomento-plantilla-consulta.component.html',
 	styleUrls: ['./ejemplo-fomento-plantilla-consulta.component.scss'],
 })
-export class EjemploFomentoPlantillaConsultaComponent implements AfterViewInit {
+export class EjemploFomentoPlantillaConsultaComponent {
 	@ViewChild(FomentoPlantillaConsultaComponent)
 	componentePlantilla!: FomentoPlantillaConsultaComponent;
 
 	api_name = 'Listado de Usuarios del API de EIT Viv';
-	hostApi = 'http://192.168.0.81:8081';
-	endpoint = 'eit-viv/api/v1/users';
-
-	constructor(private router: Router) {}
-	table_headers = Constants.EJEMPLO_TABLE_HEADER
-
+	table_headers = [
+		{ header: 'Código', field: 'codigo', visible: true },
+		{ header: 'Nombre', field: 'nombre', visible: true },
+		{ header: 'Descripción', field: 'descripcion', visible: true },
+		{ header: 'Fecha Creación', field: 'audAlta', visible: true },
+		{ header: 'Usuario Modificación', field: 'usuModifica', visible: true },
+	];
+	idTable = 1
+	form = Constants.EJEMPLO_FORMULARIO_TABLA
 	actions = true;
 	rowsPerPageOptions = [5, 10, 15];
+	hostApi = 'http://localhost:8080';
 
-	hostapiPaginator = 'http://192.168.0.81:8081/eit-viv';
-	endpointPaginator = 'api/v1/users/filter';
+	endpoint = 'api/v1/formularios/listbyquerydsl';
 
-	hostapiFilter = 'http://192.168.0.81:8081/eit-viv';
-	endpointFilter = 'api/v1/users/filter';
+	private subscription: Subscription = new Subscription();
 
-	hostapiSaveFilter = 'http://192.168.0.81:8081/customsearch';
-	endpointSaveFilter = 'api/v1/custom-search';
+	constructor(private http: HttpClient) {}
 
-	hostapiFiltroUsuarioApi = 'http://192.168.0.81:8081/customsearch';
-	endpointFiltroUsuarioApi = `api/v1/custom-search/usuario/1/listado/1/subsistema/20`;
-
-	idSubsistema = "20"
-	idTable = 1
-
-	listadoAccionesAux = Constants.EJEMPLO_LISTADO_ACCIONES_AUX
-
-
-	form = Constants.EJEMPLO_FORMULARIO_TABLA
-
-	
+		
 	descargar(datosTabla) {
 		console.log('DESCARGAR TABLA: ', datosTabla);
 	}
@@ -53,9 +45,5 @@ export class EjemploFomentoPlantillaConsultaComponent implements AfterViewInit {
 
 	check(datosSeleccionados) {
 		console.log('LAS LÍNEAS SELECCIONADAS SON: ', datosSeleccionados);
-	}
-
-	ngAfterViewInit() {
-		this.componentePlantilla?.gestionBreadcrumb(this.router.url);
 	}
 }
