0c153cd04d2c5b29e93d7589446b78991c59afea
cfernandez <cfernandez@proxya.com>
2024-10-04 09:45:00 +0200

    Accesibilidad select completo

--- fuentes/src/app/modules/componentes/ejemplo-fomento-select/ejemplo-fomento-select.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-select/ejemplo-fomento-select.component.ts
@@ -7,14 +7,14 @@ import { HttpClient } from '@angular/common/http';
 	styleUrls: ['./ejemplo-fomento-select.component.scss'],
 })
 export class EjemploFomentoSelectComponent implements OnInit {
-	data = []; // Opciones que se cargarán desde el backend
-	label = 'Selector'; // Etiqueta del select
-	disabled = false; // Si el select está deshabilitado
-	isRequired = true; // Si es requerido
-	defaultOptionText = '- Elija una opción -'; // Texto de la opción por defecto
-	additionalDescription = 'Seleccione una opción para continuar'; // Descripción accesible
-	hasError = false; // Si hay un error
-	errorMessage = ''; // Mensaje de error
+	data = []; 
+	label = 'Selector'; 
+	disabled = false; 
+	isRequired = true;
+	defaultOptionText = '- Elija una opción -'; 
+	additionalDescription = 'Seleccione una opción para continuar'; 
+	hasError = false; 
+	errorMessage = ''; 
 
 	constructor(private http: HttpClient) {}
 
@@ -22,7 +22,6 @@ export class EjemploFomentoSelectComponent implements OnInit {
 		this.fetchSelectOptions();
 	}
 
-	// Cargar las opciones del select desde el backend
 	fetchSelectOptions() {
 		const endpoint = 'http://localhost:8080/api/c1/v1/formularios/list'; 
 
@@ -43,15 +42,12 @@ export class EjemploFomentoSelectComponent implements OnInit {
 		);
 	}
 
-	// Manejador de errores
 	handleError(message: string) {
 		this.hasError = true;
 		this.errorMessage = message;
 	}
 
-	// Manejador de cambio de opción
 	onOptionChange(selectedOption: string) {
 		console.log('Opción seleccionada:', selectedOption);
-		// Aquí puedes manejar lo que ocurre al cambiar de opción
 	}
 }
