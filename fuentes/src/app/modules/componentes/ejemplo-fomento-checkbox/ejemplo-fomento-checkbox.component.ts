6c321acf95fc4db11e37ee0d8dc39885f7e6d35e
Alejandro Rodríguez <ahirtle@proxya.com>
2024-10-17 08:21:12 +0000

    Resolve "IAF-HU-002 TA-002 [TEST] Implementar accesibilidad componente checkbox"

--- fuentes/src/app/modules/componentes/ejemplo-fomento-checkbox/ejemplo-fomento-checkbox.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-checkbox/ejemplo-fomento-checkbox.component.ts
@@ -6,9 +6,27 @@ import { Component } from '@angular/core';
 	styleUrls: ['./ejemplo-fomento-checkbox.component.scss'],
 })
 export class EjemploFomentoCheckboxComponent {
-	typeStyle = 'material';
-	checked = false;
-	disabled = false;
-	name = 'chx';
+	typeStyle = 'material'; 
+	checked = false; 
+	disabled = false; 
+	readonly = false; 
+	name = 'chx'; 
 	label = 'Marcar opción';
+	ariaLabel = 'Seleccionar esta opción'; 
+	ariaDescribedBy = 'description-id'; 
+	errorMessage = ''; 
+	errorLive = true;
+
+	// Aquí definimos la propiedad `value` para pasarla al componente hijo
+	value = 'checkboxValue';
+
+	// Función que maneja el evento de cambio del checkbox
+	handleCheck(checked: boolean) {
+		this.checked = checked;
+		if (!this.checked) {
+			this.errorMessage = 'Debe marcar esta opción.';
+		} else {
+			this.errorMessage = ''; 
+		}
+	}
 }
