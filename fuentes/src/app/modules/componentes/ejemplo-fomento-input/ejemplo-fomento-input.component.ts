afe2ffeea809ca788aa18273d1e53dd88a17a875
cfolia <cfolia@emergya.com>
2024-10-18 08:44:59 +0000

    Solucion MR

--- fuentes/src/app/modules/componentes/ejemplo-fomento-input/ejemplo-fomento-input.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-input/ejemplo-fomento-input.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ejemplo-fomento-input',
@@ -7,8 +6,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./ejemplo-fomento-input.component.scss'],
 })
 export class EjemploFomentoInputComponent {
-  ejemploForm: FormGroup;
-
   // Atributos para el input
   typeStyle = 'material';
   msgError = '';
@@ -23,36 +20,8 @@ export class EjemploFomentoInputComponent {
   required = true;
   placeholder = 'Correo Electrónico';
 
-  constructor(private fb: FormBuilder) {
-    // Inicialización del formulario reactivo
-    this.ejemploForm = this.fb.group({
-      correo: ['', [Validators.required, Validators.email]],
-    });
+  constructor() {
 
-    // Suscribirse a los cambios de estado para manejar errores
-    this.ejemploForm.statusChanges.subscribe(() => {
-      this.setValidationMessages();
-    });
-  }
-
-  // Función para manejar el cambio en el valor del input
-  onInputChange(value: string): void {
-    this.valueMatter = value;
-    this.setValidationMessages();
-  }
-
-  // Función para establecer mensajes de error según la validación del formulario
-  private setValidationMessages(): void {
-    const correoControl = this.ejemploForm.get('correo');
-    if (correoControl?.invalid && correoControl?.touched) {
-      if (correoControl.errors?.['required']) {
-        this.msgError = 'El correo es requerido.';
-      } else if (correoControl.errors?.['email']) {
-        this.msgError = 'Debe ser un correo electrónico válido.';
-      }
-    } else {
-      this.msgError = '';
-    }
   }
 
 }
