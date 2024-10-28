1cd32e0c7d77854520d407822dfa0aeb7aaa7863
abenitez <abenitez@emergya.com>
2024-10-28 11:25:51 +0100

    Adiccion de nuevos input

--- fuentes/src/app/modules/componentes/ejemplo-fomento-button-toggle/ejemplo-fomento-button-toggle.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-button-toggle/ejemplo-fomento-button-toggle.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { MatButtonToggleAppearance, MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
 	selector: 'app-ejemplo-fomento-button-toggle',
@@ -10,7 +10,7 @@ export class EjemploFomentoButtonToggleComponent {
 	@Output() onclickevent = new EventEmitter<MatButtonToggleChange>();
 
 	//Propiedades mat-button-toggle
-	appearance = 'standar';
+	appearance: MatButtonToggleAppearance = 'standard';
 	label = 'button toggle';
 	disabled = false;
 	ariaLabel = '';
@@ -18,8 +18,46 @@ export class EjemploFomentoButtonToggleComponent {
 	checked = false;
 	disableRipple = true;
 	id = '';
-	value = '';
-	name = '';
+	value = 'bold';
+	name = 'bold';
+	vertical = true;
+	multiple = false;
+
+	toggleGroup = [
+		{
+		  disabled: false,
+		  ariaLabel: '',
+		  ariaLabelledby: '',
+		  checked: false,
+		  disabledRipple: true,
+		  id: 'jose',
+		  value: 'jose',
+		  name: 'toggleGroup',
+		  label: 'Jose',
+		},
+		{
+		  disabled: true,
+		  ariaLabel: '',
+		  ariaLabelledby: '',
+		  checked: false,
+		  disabledRipple: true,
+		  id: 'emilio',
+		  value: 'emilio',
+		  name: 'toggleGroup',
+		  label: 'Emilio',
+		},
+		{
+		  disabled: false,
+		  ariaLabel: '',
+		  ariaLabelledby: '',
+		  checked: true, 
+		  disabledRipple: true,
+		  id: 'antonio',
+		  value: 'antonio',
+		  name: 'toggleGroup',
+		  label: 'Antonio',
+		},
+	  ];
 
 	clickbutton(event) {
 		this.onclickevent.emit(event);
