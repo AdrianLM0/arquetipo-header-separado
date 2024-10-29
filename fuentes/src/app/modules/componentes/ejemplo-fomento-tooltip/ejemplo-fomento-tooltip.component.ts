1682a139837cccdb6b234bbe908c6dd353584577
ahirtle <ahirtle@emergya.com>
2024-10-29 15:04:39 +0000

    Parametrización tooltip

--- fuentes/src/app/modules/componentes/ejemplo-fomento-tooltip/ejemplo-fomento-tooltip.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-tooltip/ejemplo-fomento-tooltip.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
 	templateUrl: './ejemplo-fomento-tooltip.component.html',
 	styleUrls: ['./ejemplo-fomento-tooltip.component.scss'],
 })
-export class EjemploFomentoTooltipComponent {}
+export class EjemploFomentoTooltipComponent {
+	// Declaración de propiedades para el tooltip
+	description: string = "Este es un tooltip de ejemplo";
+	position: 'above' | 'below' | 'left' | 'right' = 'below';
+	delay_hide: number = 500;
+	delay_show: number = 200;
+}
