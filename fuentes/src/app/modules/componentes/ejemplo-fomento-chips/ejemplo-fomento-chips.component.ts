e7fb1f9b88a7cb0ba33e39ed6dff969bdbeaad13
cfernandez <cfernandez@proxya.com>
2024-10-30 09:40:48 +0100

    fix estilos chips

--- fuentes/src/app/modules/componentes/ejemplo-fomento-chips/ejemplo-fomento-chips.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-chips/ejemplo-fomento-chips.component.ts
@@ -16,7 +16,7 @@ export class EjemploFomentoChipsComponent {
 	selected;
 	chips = [
         { id: '1', label: 'Chip 1', value: '1', color: 'primary' as ThemePalette, selected: true, ariaLabel: 'Botón chip 1', ariaDescrption: 'Boton chip con valor 1' },
-        { id: '2', label: 'Chip 2', value: '2', color: 'accent' as ThemePalette, selected: false, disabled: true, ariaLabel: 'Botón chip 2', ariaDescrption: 'Boton chip deshabilitado con valor 2'},
+        { id: '2', label: 'Chip 2', value: '2', color: 'accent' as ThemePalette, selected: true, disabled: true, ariaLabel: 'Botón chip 2', ariaDescrption: 'Boton chip deshabilitado con valor 2'},
         { id: '3', label: 'Chip 3', value: '3', color: 'warn' as ThemePalette, selected: false, ariaLabel: 'Botón chip 3', ariaDescrption: 'Boton chip con valor 3' },
     ];
 }
