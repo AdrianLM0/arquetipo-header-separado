6d6a08b36b0ea279c99e0095c95ba8dff0ab80ce
Adrian <almoreno@emergya.com>
2024-11-13 18:09:10 +0100

    Cambio de lógica para que la petición se haga en test

--- fuentes/src/app/modules/componentes/ejemplo-fomento-formulario/ejemplo-fomento-formulario.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-formulario/ejemplo-fomento-formulario.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Constants } from '../../../config/constants';
+import { RequestApiService } from '@fomento/i-rf-logic-component-node-lib';
 
 @Component({
 	selector: 'app-ejemplo-fomento-formulario',
@@ -7,19 +8,25 @@ import { Constants } from '../../../config/constants';
 	styleUrls: ['./ejemplo-fomento-formulario.component.scss'],
 })
 export class EjemploFomentoFormularioComponent {
+	endpointUrl="" //Endpoint que hará la función de guardar los datos del formulario al pulsar el botón de 'submit'
 	form_config = Constants.EJEMPLO_FORMULARIO;
 	reset_button = 'BORRAR';
 	submit_button = 'GUARDAR';
 	showSubmit = true;
 	showReset = true;
 
-	prueba(data) {
-		console.log('FORMULARIO RECIBIDO', data);
-	}
-	dataSaved(data){
-		console.log('DATOS GUARDADOS', data);
-	}
+	constructor(private requestApi: RequestApiService) { }
+
 	dataEvent(data){
-		console.log('EVENTO DE DATOS', data);
+		// Hacer la petición POST usando RequestApiService
+		this.requestApi.post(this.endpointUrl, data).subscribe({
+			next: (response) => {
+	  
+			  console.log('DATOS GUARDADOS', data); 
+			},
+			error: (error) => {
+	  
+			}
+		  });
 	}
 }
