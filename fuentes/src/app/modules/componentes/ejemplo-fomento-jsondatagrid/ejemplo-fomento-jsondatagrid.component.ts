85be3612bed21cdc7f8a2a78a7535b40b31ae4df
abenitez <abenitez@emergya.com>
2024-11-26 14:09:37 +0100

    Añadir input textos

--- fuentes/src/app/modules/componentes/ejemplo-fomento-jsondatagrid/ejemplo-fomento-jsondatagrid.component.ts
+++ fuentes/src/app/modules/componentes/ejemplo-fomento-jsondatagrid/ejemplo-fomento-jsondatagrid.component.ts
@@ -55,6 +55,11 @@ export class EjemploFomentoJsonDatagridComponent implements OnDestroy {
 		],
 	};
 
+
+	helpButtonAriaLabel = '¿Necesitas ayuda?'; // NUEVO INPUT
+	globalFilterLabel = 'Buscar en todos los campos'; // NUEVO INPUT
+	dialogTitle = 'Título personalizado para el diálogo'; // NUEVO INPUT
+
 	private subscription: Subscription = new Subscription(); // Propiedad para la subscripción
 	constructor() {}
 
